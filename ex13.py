e32e2468a1af5493252f7d5e543705c95117f1eb
Fagner Cardoso <143296692+Fagner-c@users.noreply.github.com>
2025-06-05 20:28:19 -0300

    Delete ex13.py

--- ex13.py
+++
@@ -1,7 +0,0 @@
-class Veiculo():
-    def __init__(self, velo_max):
-        self.__velo_Max = velo_max
-    def get_velo_max(self):
-        return self.__velo_Max
-carro1 = Veiculo(123)
-print(carro1.get_velo_max())
