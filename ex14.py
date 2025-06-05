cc1909f54c378d8e98268ed9bbcbd3212cad4462
Fagner Cardoso <143296692+Fagner-c@users.noreply.github.com>
2025-06-05 20:28:30 -0300

    Delete ex14.py

--- ex14.py
+++
@@ -1,8 +0,0 @@
-class Computador():
-    def __init__(self, memoria):
-        self.__memoria = memoria
-    def set_memoria(self, memoria):
-        if memoria > 0:
-            self.__memoria = memoria
-        else:
-            print('Valor errado!')
